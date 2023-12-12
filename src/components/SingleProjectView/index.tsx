0362a7a22082129fdfc31c2a77b63b4ffe782b60
Tomasz Suski <tomasz.suski@orba.co>
2023-12-12 10:53:14 +0100

    some styling changes

--- src/components/SingleProjectView/index.tsx
+++ src/components/SingleProjectView/index.tsx
@@ -14,27 +14,33 @@ export default function SingleProjectView({
 }: SingleProjectViewProps) {
   return (
     <div className="flex flex-col items-center justify-center w-full gap-4">
-      <h1 className="text-2xl font-bold text-stone-800">{project.title}</h1>
-      <p className="text-sm text-stone-600">{project.description}</p>
-      <p className="text-sm text-stone-600">
-        {project.dueDate.toLocaleDateString()}
-      </p>
-      <menu className="flex gap-4">
-        <button
-          className="px-6 py-2 mt-4 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-          type="button"
-          onClick={closeHandler}
-        >
-          Close
-        </button>
-        <button
-          className="px-6 py-2 mt-4 bg-red-800 rounded-md text-stone-50 hover:bg-stone-950"
-          type="button"
-          onClick={() => deleteHandler(project.id)}
-        >
-          Delete
-        </button>
-      </menu>
+      <div className="flex flex-col w-1/4 gap-4">
+        <h1 className="text-2xl font-bold text-center text-stone-800">{project.title}</h1>
+        <p className="text-sm text-left text-stone-600">
+          {project.description}
+        </p>
+        <p className="text-sm text-center text-stone-600">
+          {typeof project.dueDate === "string"
+            ? project.dueDate
+            : project.dueDate.toLocaleDateString()}
+        </p>
+        <menu className="flex gap-4 mx-auto">
+          <button
+            className="px-6 py-2 mt-4 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            type="button"
+            onClick={closeHandler}
+          >
+            Close
+          </button>
+          <button
+            className="px-6 py-2 mt-4 bg-red-800 rounded-md text-stone-50 hover:bg-stone-950"
+            type="button"
+            onClick={() => deleteHandler(project.id)}
+          >
+            Delete
+          </button>
+        </menu>
+      </div>
     </div>
   );
 }
--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -24,8 +24,12 @@ const Modal = forwardRef(function Modal(
   return createPortal(
     <dialog ref={dialog} className="p-4 rounded-lg">
       {children}
-      <form method="dialog" onSubmit={onModalClose} className="w-full mt-4">
-        <button className="px-6 py-2 rounded-md bg-stone-600 text-stone-200 hover:bg-stone-950 hover:text-stone-50">
+      <form
+        method="dialog"
+        onSubmit={onModalClose}
+        className="flex w-full mt-4"
+      >
+        <button className="px-6 py-2 ml-auto rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
           {closeBtnText || "Close"}
         </button>
       </form>
